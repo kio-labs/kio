c4617728c8ca956e5a1aed890491286cf7809138
andannn <jqn296763005@gmail.com>
2026-08-08 23:26:01 +0900

    implement openFileSink method

--- kio-async/async-io/src/commonMain/kotlin/kio/async/io/File.kt
+++ kio-async/async-io/src/commonMain/kotlin/kio/async/io/File.kt
@@ -1,5 +1,7 @@
 package kio.async.io
 
+import kio.async.AsyncRawSink
 import kio.async.AsyncRawSource
 
 expect suspend fun openFileSource(path: String): AsyncRawSource
+expect suspend fun openFileSink(path: String): AsyncRawSink
--- kio-async/async-io/src/jvmMain/kotlin/kio/async/io/File.jvm.kt
+++ kio-async/async-io/src/jvmMain/kotlin/kio/async/io/File.jvm.kt
@@ -1,5 +1,6 @@
 package kio.async.io
 
+import kio.async.AsyncRawSink
 import kio.async.AsyncRawSource
 import kotlinx.io.Buffer
 import kotlinx.io.files.Path
@@ -15,6 +16,24 @@ actual suspend fun openFileSource(path: String): AsyncRawSource {
 
         override suspend fun readAtMostTo(sink: Buffer, byteCount: Long): Long {
             return source.readAtMostTo(sink, byteCount)
+        }
+    }
+}
+
+actual suspend fun openFileSink(path: String): AsyncRawSink {
+    val fileSink = SystemFileSystem.sink(Path(path))
+
+    return object : AsyncRawSink {
+        override suspend fun write(source: Buffer, byteCount: Long) {
+            return fileSink.write(source, byteCount)
+        }
+
+        override suspend fun flush() {
+            fileSink.flush()
+        }
+
+        override suspend fun close() {
+            fileSink.close()
         }
     }
 }
