626a835f7e61855f8713c8936746a3c314c3ad55
andannn <jqn296763005@gmail.com>
2026-08-13 22:08:05 +0900

    parse request cookie

--- kio-http/src/commonMain/kotlin/kio/http/HttpCall.kt
+++ kio-http/src/commonMain/kotlin/kio/http/HttpCall.kt
@@ -6,6 +6,7 @@ import io.ktor.http.HeadersBuilder
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpProtocolVersion
 import io.ktor.http.Parameters
+import io.ktor.http.parseClientCookiesHeader
 import io.ktor.http.parseQueryString
 import kio.async.AsyncRawSource
 import kio.async.AsyncSink
@@ -65,6 +66,16 @@ class CallContext internal constructor(
 
 suspend fun CallContext.receiveFormParameters(): Parameters {
     return parseQueryString(requestBody.readString())
+}
+
+fun CallContext.requestCookies(): Map<String, String> {
+    val cookieHeaders = requestHeaders.getAll("Cookie") ?: return emptyMap()
+    val map = HashMap<String, String>(cookieHeaders.size)
+    for (cookieHeader in cookieHeaders) {
+        val cookies = parseClientCookiesHeader(cookieHeader)
+        map.putAll(cookies)
+    }
+    return map
 }
 
 /**
